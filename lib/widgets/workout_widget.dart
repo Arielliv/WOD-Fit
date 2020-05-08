64ce2396eb7052de9c5806be3ec23379cfbfa9d6
ariel7342 <ariell@wix.com>
2020-05-08 18:22:19 +0300

    fix build

--- lib/widgets/workout_widget.dart
+++ lib/widgets/workout_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:wod_fit/screens/workout_detail_screen.dart';
-import 'package:wod_fit/screens/workout_details_screen.dart';
 import '../providers/workout.dart';
 import '../providers/auth.dart';
 
